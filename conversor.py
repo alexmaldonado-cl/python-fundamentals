e32bb9f4248b677dc14ee0650e115a0a3e8fa62f
Alex Maldonado <alex.maldonado@outlook.com>
2021-03-11 20:50:11 -0300

    add new currency exchange options

--- conversor.py
+++ conversor.py
@@ -1,7 +1,41 @@
-pesos = input("¿Cuántos pesos tienes?:  ")
-pesos = float(pesos)
-valor_dolar = 724.40
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dólares")
+menu = """
+Bienvenido al conversor de monedas 💰
+
+==== Pesos Chilenos ====
+
+1 - Convertir a Dólares
+2 - Convertir a UF
+3 - Convertir a Euros
+
+Elige un opción:
+
+"""
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input("¿Cuántos pesos tienes?:  ")
+    pesos = float(pesos)
+    valor_dolar = 724.40
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 2:
+    pesos = input("¿Cuántos pesos tienes?:  ")
+    pesos = float(pesos)
+    valor_uf = 29356.90
+    dolares = pesos / valor_uf
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes " + dolares + " UF")
+elif opcion == 3:
+    pesos = input("¿Cuántos pesos tienes?:  ")
+    pesos = float(pesos)
+    valor_euro = 854.89
+    dolares = pesos / valor_euro
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes " + dolares + " euros")
+else:
+    print('Ingresa una opción correcta')
